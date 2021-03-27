912f02980108f5e720839053fe369460899a141a
vdval <vdval@users.noreply.github.com>
2021-03-27 19:22:43 +0100

    Création README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+Ceci est un premier jet pour un programme graphique avec swing.
+Pour l'instant, il y a une seule fenêtre mais on voit le déroulé du code.
+Je travaille sous IntelliJ Ultimate.
--- src/fr/anakin/app/Main.java
+++ src/fr/anakin/app/Main.java
@@ -11,7 +11,11 @@ public class Main {
     cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     cadre.getContentPane().add(bouton);
-    cadre.setSize(300, 300);
+    cadre.setSize(400, 300);
     cadre.setVisible(true);
+  }
+
+  public void modifier() {
+    bouton.setText("J'ai été cliqué !");
   }
 }
