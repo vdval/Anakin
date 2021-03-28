d5a6f624c43665dd837d4482197252ed88a24622
vdval <vdval@users.noreply.github.com>
2021-03-28 08:39:43 +0200

    modification Main.java en totalité avec rajout de AWT

--- src/fr/anakin/app/Main.java
+++ src/fr/anakin/app/Main.java
@@ -1,21 +1,33 @@
 package fr.anakin.app;
 
 import javax.swing.*;
+import java.awt.event.*;
 
-public class Main {
+public class Main implements ActionListener {
+
+  JButton bouton;
 
   public static void main(String[] args) {
+
+    Main ihm = new Main();
+    ihm.go();
+
+  }
+
+  public void go() {
     JFrame cadre = new JFrame();
     JButton bouton = new JButton("cliquez moi");
 
-    cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    bouton.addActionListener(this);zzzzzzqaerrr
 
     cadre.getContentPane().add(bouton);
+    cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     cadre.setSize(400, 300);
     cadre.setVisible(true);
   }
 
-  public void modifier() {
+  public void actionPerformed(ActionEvent event) {
+
     bouton.setText("J'ai été cliqué !");
   }
 }
